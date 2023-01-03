4e22e76501467b7b886ea4ef8fc1b88964685c11
houdamzari <houdamzari1999@gmail.com>
2023-01-03 20:22:02 +0100

    Fix Eslint issues

--- src/fetchData.js
+++ src/fetchData.js
@@ -1,58 +1,51 @@
+/* eslint-disable no-use-before-define */
 import axios from 'axios';
 
 let element = '';
 const grid = document.querySelector('.grid');
 axios
   .post(
-    'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps'
+    'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps',
   )
   .then((res) => localStorage.setItem('appId', res.data));
-const gridLogic = (data, i) => {
+const gridLogic = (data) => {
   const appId = localStorage.getItem('appId');
   const numberlikes = JSON.parse(localStorage.getItem('likes'));
-  let postLikes = Array.isArray(numberlikes) ? numberlikes : [];
+  const postLikes = Array.isArray(numberlikes) ? numberlikes : [];
   let postLikeNumber;
-  postLikes.forEach((like) =>
-    like.item_id == data.id ? (postLikeNumber = like.likes) : ''
-  );
+  // eslint-disable-next-line no-return-assign
+  postLikes.forEach((like) => (parseInt(like.item_id, 10) === data.id ? (postLikeNumber = like.likes) : ''));
 
-  console.log(postLikes);
   axios
     .get(
-      `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${appId}/likes`
+      `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${appId}/likes`,
     )
     .then((res) => localStorage.setItem('likes', JSON.stringify(res.data)));
   element += `<div class="grid-item">
   <div class="imageContainer"><img src=${data.sprites.front_default} /></div>
   <h3>${data.species.name}</h3>
-  ${postLikeNumber ? postLikeNumber : ''}<button class="likeButton" id=${
-    data.id
-  }>like</button>
+  ${postLikeNumber || ''}<button class="likeButton" id=${data.id}>like</button>
   <div class="buttonContainer"><button>Comments</button><button>Reservation</button></div>
   </div>`;
   grid.innerHTML = element;
   const likeButton = document.querySelectorAll('.likeButton');
   const likingHandle = () => {
-    likeButton.forEach((button) =>
-      button.addEventListener('click', () => {
-        const appId = localStorage.getItem('appId');
-        axios
-          .post(
-            `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${appId}/likes`,
-            { item_id: button.id }
-          )
-          .then((res) =>
-            setTimeout(() => {
-              element = '';
-              fetchData();
-            }, 1000)
-          );
-        setTimeout(() => {
+    likeButton.forEach((button) => button.addEventListener('click', () => {
+      const appId = localStorage.getItem('appId');
+      axios
+        .post(
+          `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${appId}/likes`,
+          { item_id: button.id },
+        )
+        .then(() => setTimeout(() => {
           element = '';
           fetchData();
-        }, 1000);
-      })
-    );
+        }, 1000));
+      setTimeout(() => {
+        element = '';
+        fetchData();
+      }, 1000);
+    }));
   };
   likingHandle();
 };
